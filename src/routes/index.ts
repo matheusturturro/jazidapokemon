58f1b6725ab4360a7d49afe60e8e40cae35d17d1
Matheusturturro <matheusturturro@gmail.com>
2025-05-14 23:40:27 -0300

    ultimas tentativas de arrumar o front. falhei

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,3 +6,9 @@ const router = express.Router();
 router.use('/', pokemonRouter);
 
 export default router;
+
+
+//Criamos um roteador isolado (router).
+// Inserimos nele todas as rotas de Pokémon (pokemonRouter) a partir do caminho raiz ('/').
+// Exportamos esse roteador composto para ser usado no ponto de entrada da API, mantendo
+//  a organização e separação de responsabilidades entre os diversos grupos de rotas.
--- src/routes/pokemonRoutes.ts
+++ src/routes/pokemonRoutes.ts
@@ -12,3 +12,8 @@ router.delete('/pokemons/:id', PokemonController.deletePokemon as unknown as Req
 router.post('/batalhar/:pokemonAId/:pokemonBId', BatalhaController.battlePokemons as unknown as RequestHandler);
 
 export default router;
+
+//Criamos um roteador isolado (router).
+// Inserimos nele todas as rotas de Pokémon (pokemonRouter) a partir do caminho raiz ('/').
+// Exportamos esse roteador composto para ser usado no ponto de entrada da API, mantendo
+//  a organização e separação de responsabilidades entre os diversos grupos de rotas.
